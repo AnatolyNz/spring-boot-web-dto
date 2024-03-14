0b0f1280f1cb3b421d8e997ba9ab197e1bfe15a6
Anatolii Nazarov <anatoly_n@ukr.net>
2024-03-14 10:58:52 +0200

    For check SpringBootJwt

--- src/main/java/mate/academy/controller/AuthenticationController.java
+++ src/main/java/mate/academy/controller/AuthenticationController.java
@@ -2,9 +2,12 @@ package mate.academy.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.dto.UserLoginRequestDto;
+import mate.academy.dto.UserLoginResponseDto;
 import mate.academy.dto.UserRegistrationRequestDto;
 import mate.academy.dto.UserResponseDto;
 import mate.academy.exception.RegistrationException;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
