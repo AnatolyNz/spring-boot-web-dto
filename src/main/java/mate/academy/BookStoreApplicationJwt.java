fa93d106610e1299739d8a4ecdc7e401b7587821
Anatolii Nazarov <anatoly_n@ukr.net>
2024-03-14 11:07:09 +0200

    For check SpringBootJwt

--- src/main/java/mate/academy/BookStoreApplicationJwt.java
+++ src/main/java/mate/academy/BookStoreApplicationJwt.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplicationJwt {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookStoreApplication.class, args);
+        SpringApplication.run(BookStoreApplicationJwt.class, args);
     }
 }
