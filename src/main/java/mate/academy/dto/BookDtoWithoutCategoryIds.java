2473e4f2e2f6d58dc21b8251dc456ef353273c85
Anatolii Nazarov <anatoly_n@ukr.net>
2024-03-21 11:56:52 +0200

    For check SpringBootPractice

---
+++ src/main/java/mate/academy/dto/BookDtoWithoutCategoryIds.java
@@ -0,0 +1,19 @@
+package mate.academy.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.math.BigDecimal;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Accessors(chain = true)
+public class BookDtoWithoutCategoryIds {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
