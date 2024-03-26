3b0cfba4beb4133fdf94d5c21580b8bc76ecfc3d
Anatolii Nazarov <anatoly_n@ukr.net>
2024-03-26 10:51:39 +0200

    For check SpringBootPractice2

---
+++ src/main/java/mate/academy/controller/ShoppingCartController.java
@@ -0,0 +1,69 @@
+package mate.academy.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import mate.academy.dto.CartItemQuantityRequestDto;
+import mate.academy.dto.CartItemRequestDto;
+import mate.academy.dto.ShoppingCartDto;
+import mate.academy.repository.CartItemRepository;
+import mate.academy.service.ShoppingCartService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Shopping cart management", description = "Shopping cart for managing categories")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/cart")
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService;
+    private final CartItemRepository cartItemRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add new book to cart",
+            description = "Before adding new book we check if this book is already in cart "
+                    + "and than or change qty or add new cartItem")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public void addBook(Authentication authentication,
+                        @RequestBody @Valid CartItemRequestDto cartItemRequestDto) {
+        shoppingCartService.addItemToCart(authentication, cartItemRequestDto);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @Operation(summary = "Get all books from cart", description = "Get all books from cart")
+    public ShoppingCartDto getAllCartItems(Authentication authentication) {
+        return shoppingCartService.getAllCartItems(authentication);
+    }
+
+    @PutMapping("/cart-items/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update book qty in cart",
+            description = "You just set new qty of book in your shopping cart")
+    public void updateCartItemByBookId(Authentication authentication, @PathVariable Long id,
+                                       @RequestBody @Valid
+                                       CartItemQuantityRequestDto qty) {
+        shoppingCartService.updateBookQuantity(authentication, id, qty);
+    }
+
+    @DeleteMapping("/cart-items/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Delete cart item", description = "Delete cart item")
+    public void removeCartItemByBookId(Authentication authentication, @PathVariable Long id) {
+        cartItemRepository.deleteById(id);
+    }
+}
